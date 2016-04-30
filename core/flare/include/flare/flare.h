d8eadb65c7d777a6cc2b4c4de23b05dfe5b6d15b
Dreaded_X <tim.huizinga@gmail.com>
2016-04-30 15:45:58 +0200

    Removed dependecy on old model loader

--- core/flare/include/flare/flare.h
+++ core/flare/include/flare/flare.h
@@ -13,10 +13,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
-
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
