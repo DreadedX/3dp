22ffe22a126007817e62e7437b124e74cdd196d1
Dreaded_X <tim.huizinga@gmail.com>
2016-07-15 01:14:59 +0200

    Started work on small build control tool

--- tools/launcher/src/launcher.cpp
+++ tools/launcher/src/launcher.cpp
@@ -1,58 +1,19 @@
-#include <ncurses.h>
+#include <iostream>
 
 int main() {
 
-	WINDOW *w;
-	char list[4][20] = { "Build Linux Debug", "Build Linux Release", "Build Windows Debug", "Build Linux Release" };
-	char item[20];
-	int ch, i = 0, width = 20;
+	std::cout << "Launcher tool" << std::endl;
 
-	initscr(); // initialize Ncurses
-	w = newwin( 10, 42, 1, 1 ); // create a new window
-	box( w, 0, 0 ); // sets default borders for the window
+	int option = 0; 
 
-	// now print all the menu items and highlight the first one
-	for( i=0; i<4; i++ ) {
-		if( i == 0 ) 
-			wattron( w, A_BOLD ); // highlights the first item.
-		else
-			wattroff( w, A_BOLD );
-		sprintf(item, "%-20s",  list[i]);
-		mvwprintw( w, i+1, 2, "%s", item );
-	}
+	do {
 
-	wrefresh( w ); // update the terminal screen
+		std::cout << "Input: ";
+		std::cin >> option;
 
-	i = 0;
-	noecho(); // disable echoing of characters on the screen
-	keypad( w, TRUE ); // enable keyboard input for the window.
-	curs_set( 0 ); // hide the default screen cursor.
+		std::cout << "Selected: " << option << std::endl;
 
-	// get the input
-	while(( ch = wgetch(w)) != 'q'){ 
+	} while (option != 4);
 
-		// right pad with spaces to make the items appear with even width.
-		sprintf(item, "%-20s",  list[i]); 
-		mvwprintw( w, i+1, 2, "%s", item ); 
-		// use a variable to increment or decrement the value based on the input.
-		switch( ch ) {
-			case 107:
-				i--;
-				i = ( i<0 ) ? 3 : i;
-				break;
-			case 106:
-				i++;
-				i = ( i>3 ) ? 3 : i;
-				break;
-		}
-		// now highlight the next item in the list.
-		wattron( w, A_BOLD );
-
-		sprintf(item, "%-20s",  list[i]);
-		mvwprintw( w, i+1, 2, "%s", item);
-		wattroff( w, A_BOLD );
-	}
-
-	delwin( w );
-	endwin();
+	std::cout << "Exiting" << std::endl;
 }
