48bb0bd26873d21cfed2623754225ea79196c109
Dreaded_X <tim.huizinga@gmail.com>
2016-07-15 00:56:16 +0200

    Enabled OpenGL version requirement again

--- core/flare/src/engine.cpp
+++ core/flare/src/engine.cpp
@@ -56,11 +56,10 @@ void flare::init() {
 
 	glfwSetErrorCallback(errorCallbackGLFW);
 
-	/** @note Disabled for testing */
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	// glfwWindowHint(GLFW_SAMPLES, 4);
 
